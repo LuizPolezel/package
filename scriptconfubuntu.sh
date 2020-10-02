28cd6711772f76c79b7fa73848bc162cc6ff79be
LuizPolezel <luizpolezel@outlook.com>
2020-10-02 15:42:23 -0300

    Update do script.

--- scriptconfubuntu.sh
+++ scriptconfubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Update Ubuntu:"
+sudo apt-get update -y
+
 echo "Instalação Sophos:"
 wget https://api-cloudstation-us-east-2.prod.hydra.sophos.com/api/download/3f3989c05a7db62d3c2f7327f2815c40/SophosInstall.sh
 sudo chmod 777 SophosInstall.sh
@@ -16,7 +19,7 @@ sudo systemctl enable konea
 
 echo "Integração AD Endurance:"
 wget https://github.com/BeyondTrust/pbis-open/releases/download/8.5.7/pbis-open-8.5.7.385.linux.x86_64.deb.sh
-sudo apt install openssh-server
+sudo apt install openssh-server -y
 sudo sh pbis-open-8.5.7.385.linux.x86_64.deb.sh
 sudo /opt/pbis/bin/domainjoin-cli join CORP.ENDURANCE.COM
 
